26d67d02cae4244dcef0ecb481f83709e2179918
Anton <antonbad03@gmail.com>
2022-05-20 02:11:16 +0300

    -change pictures(sun,rain,cloud) depending on the weather

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -32,13 +33,18 @@ public class MainActivity extends AppCompatActivity {
     private TextView fl_spb;
     private TextView wind_spb;
     private TextView time_spb;
-
+    private ImageView cloud_spb;
+    private ImageView rain_spb;
+    private ImageView sun_spb;
 
     private TextView descr_tsk;
     private TextView temp_tsk;
     private TextView fl_tsk;
     private TextView wind_tsk;
     private TextView time_tsk;
+    private ImageView cloud_tsk;
+    private ImageView rain_tsk;
+    private ImageView sun_tsk;
 
     private ViewFlipper viewFlipper;
 
@@ -59,15 +65,26 @@ public class MainActivity extends AppCompatActivity {
 
         temp_spb = findViewById(R.id.temp_spb);
         temp_tsk = findViewById(R.id.temp_tsk);
+
         fl_spb = findViewById(R.id.fl_spb);
         fl_tsk = findViewById(R.id.fl_tsk);
+
         wind_spb = findViewById(R.id.wind_spb);
         wind_tsk = findViewById(R.id.wind_tsk);
+
         descr_tsk = findViewById(R.id.descr_tsk);
         descr_spb = findViewById(R.id.descr_spb);
+
         time_tsk = findViewById(R.id.time_tsk);
         time_spb = findViewById(R.id.time_spb);
 
+        cloud_tsk = findViewById(R.id.cloud_tsk);
+        rain_tsk = findViewById(R.id.rain_tsk);
+        sun_tsk = findViewById(R.id.sun_tsk);
+
+        cloud_spb = findViewById(R.id.cloud_spb);
+        rain_spb = findViewById(R.id.rain_spb);
+        sun_spb = findViewById(R.id.sun_spb);
 
         String url = "https://api.openweathermap.org/data/2.5/weather?lat=56.501041&lon=84.992455&appid=be59c27f4f9fe68c807fdd8034c9e97b&units=metric&lang=en";
         String url_spb = "https://api.openweathermap.org/data/2.5/weather?lat=59.937500&lon=30.308611&appid=be59c27f4f9fe68c807fdd8034c9e97b&units=metric&lang=en";
@@ -157,6 +174,16 @@ public class MainActivity extends AppCompatActivity {
                 fl_tsk.setText((int)jsonObject.getJSONObject("main").getDouble("feels_like")+"°C");
                 wind_tsk.setText((int)jsonObject.getJSONObject("wind").getDouble("speed")+" m/s");
                 descr_tsk.setText((CharSequence) jsonObject.getJSONArray("weather").getJSONObject(0).getString("description"));
+
+                if(descr_tsk.getText().equals("clear sky")){
+                    sun_tsk.setVisibility(View.VISIBLE);
+                }else if(descr_tsk.getText().equals("few clouds")
+                        ||descr_tsk.getText().equals("scattered clouds")
+                        ||descr_tsk.getText().equals("broken clouds")){
+                    cloud_tsk.setVisibility(View.VISIBLE);
+                }else{
+                    rain_tsk.setVisibility(View.VISIBLE);
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -218,6 +245,16 @@ public class MainActivity extends AppCompatActivity {
                    fl_spb.setText((int)jsonObject.getJSONObject("main").getDouble("feels_like")+"°C");
                    wind_spb.setText((int)jsonObject.getJSONObject("wind").getDouble("speed")+" m/s");
                    descr_spb.setText((CharSequence) jsonObject.getJSONArray("weather").getJSONObject(0).getString("description"));
+
+                   if(descr_spb.getText().equals("clear sky")){
+                       sun_spb.setVisibility(View.VISIBLE);
+                   }else if(descr_spb.getText().equals("few clouds")
+                           ||descr_spb.getText().equals("scattered clouds")
+                           ||descr_spb.getText().equals("broken clouds")){
+                       cloud_spb.setVisibility(View.VISIBLE);
+                   }else{
+                       rain_spb.setVisibility(View.VISIBLE);
+                   }
                } catch (JSONException e) {
                    e.printStackTrace();
                }
